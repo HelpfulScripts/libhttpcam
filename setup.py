9b071203dfe9d048dc3d94f4426af653e537fff9
hs <helpfulscripts@gmail.com>
2019-09-29 08:05:10 -0700

    version patch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="libhttpcam",
-    version="0.1.0",
+    version="0.1.1",
     author="Helpful Scripts",
     author_email="helpfulscripts@gmail.com",
     description="Accessing webcams via REST API",
